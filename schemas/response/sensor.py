b3c2e503591f92ac47865040028e7dfc6a4dd081
unknown <ninov.vasil@googlemail.com>
2024-11-03 16:39:52 +0100

    Implement CRUD for Sensor Statistic

--- schemas/response/sensor.py
+++ schemas/response/sensor.py
@@ -3,6 +3,7 @@ from marshmallow import fields
 from schemas.base import BaseSensor
 from schemas.response.sensor_data import SensorDataResponseSchema
 from schemas.response.sensor_schedule import SensorScheduleResponseSchema
+from schemas.response.sensor_statistic import SensorStatisticResponseSchema
 
 
 # Sensor Schema
@@ -11,5 +12,5 @@ class SensorResponseSchema(BaseSensor):
     user_id = fields.Integer(required=True)
     sensor_data = fields.Nested(SensorDataResponseSchema, many=True)
     sensor_schedules = fields.Nested(SensorScheduleResponseSchema, many=True)
-    #sensor_statistics = fields.Nested(SensorStatisticResponseSchema, many=True)
+    sensor_statistics = fields.Nested(SensorStatisticResponseSchema, many=True)
 
